9de0aee6502a501e68ec9e55ec0e3c204fee8d03
pohuliaieva <bratkevich.e.a@gmail.com>
2022-11-08 21:02:15 +0000

    add link on github

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import './index.css';
 export default function Footer() {
     return (
         <div className="Footer">
-            <a href="#">By Pohuliaieva</a>
+            <a href="https://github.com/pohuliaieva/app-react">By Pohuliaieva</a>
         </div>
     );
 }
